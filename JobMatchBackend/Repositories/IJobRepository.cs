a70900434baacb856967eb6b74b88e569e3150d6
Noelitaa <noeliafallasbermudez@gmail.com>
2026-04-29 12:09:14 -0600

    edit IJobRepository

--- JobMatchBackend/Repositories/IJobRepository.cs
+++ JobMatchBackend/Repositories/IJobRepository.cs
@@ -1,10 +1,12 @@
-// Repositories/IJobRepository.cs
+// IJobRepository.cs
 using JobMatchBackend.Models.Entities;
 
 namespace JobMatchBackend.Repositories;
 
 public interface IJobRepository
 {
+    Task<Job> CreateAsync(Job job);
+    
     Task<Job?> GetByIdAsync(int id);
     Task<bool> IsCompanyOwnerAsync(int jobId, Guid companyId);
 }
