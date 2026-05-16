97e61100bd2c7144e58cfc3cbcdd5c4c4d5534a6
Noelitaa <noeliafallasbermudez@gmail.com>
2026-05-15 22:59:37 -0600

    changes controller

--- JobMatchBackend/Controllers/ApplicationsController.cs
+++ JobMatchBackend/Controllers/ApplicationsController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using JobMatchBackend.Services;
-
+using JobMatchBackend.DTOs.Request;
 namespace JobMatchBackend.Controllers;
 
 [ApiController]
@@ -34,6 +34,35 @@ public class ApplicationsController : ControllerBase
 {
     return StatusCode(500, new { message = "Internal server error" });
 }
+    }
+
+
+ // PUT: /applications/{applicationId}
+    [HttpPut("applications/{applicationId}")]
+    public async Task<IActionResult> UpdateApplicationStatus(int applicationId, [FromBody] UpdateApplicationRequest request)
+    {
+        try
+        {
+            var companyId = GetCurrentUserId();
+            var result = await _applicationService.UpdateApplicationStatusAsync(applicationId, companyId, request);
+            return Ok(result);
+        }
+        catch (KeyNotFoundException)
+        {
+            return NotFound(new { message = "Application not found" });
+        }
+        catch (UnauthorizedAccessException ex)
+        {
+            return StatusCode(403, new { message = ex.Message });
+        }
+        catch (InvalidOperationException ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+        catch (ArgumentException ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 
     private Guid GetCurrentUserId()
