43f0da5cf45ea9c945c6565b7c1dac3288619415
Carranza506 <andres.carranza.moyano@est.una.ac.cr>
2026-06-19 17:37:45 -0600

    changes

--- JobMatchBackend/DTOs/Request/UpdateDescriptionRequest.cs
+++ JobMatchBackend/DTOs/Request/UpdateDescriptionRequest.cs
@@ -5,5 +5,6 @@ namespace JobMatchBackend.DTOs.Request;
 public class UpdateDescriptionRequest
 {
     [Required]
+    [MaxLength(500)]
     public string Description { get; set; } = string.Empty;
 }
